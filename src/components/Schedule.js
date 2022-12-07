030fe9c9e103eee8fd24aab35cf635a919110f26
Mikael Josias <mikaeljosias2002@gmail.com>
2022-12-07 14:11:01 -0300

    feat: show date and time from schedule array

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,20 +1,67 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 export default function Schedule(props){
+    const [schedules, setSchedules] = useState(null);
+
     const {movie} = props;
     const scheduleUrl = `https://mock-api.driven.com.br/api/v8/cineflex/movies/${movie.id}/showtimes`;
 
-
     useEffect(() => {
         const promisse = axios.get(scheduleUrl);
-        promisse.then((p) => console.log(p.data));
+        promisse.then((p) => setSchedules(p.data.days));
         promisse.catch((p) => console.log(p.message));
     }, []);
 
+    if (schedules === null) {
+        return <div>Carregando...</div>
+    }
+
     return (
-        <>
-            Schedule
-        </>
+        <ScheduleSection>
+            {schedules.map((s) => <ScheduleOption><DaySpan>{s.weekday} - {s.date}</DaySpan><TimeOption>{s.showtimes.map((st) => <><RadioOption type="radio" id={st.id}/><LabelOption htmlFor={st.id} >{st.name}</LabelOption></>)}</TimeOption></ScheduleOption>)}
+        </ScheduleSection>
     );
 }
+
+const ScheduleSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    width: 100%;
+`;
+
+const ScheduleOption = styled.div`
+    padding-left: 22px;
+    width: 100%;
+`;
+
+const DaySpan = styled.span`
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+    font-weight: 400;
+`;
+
+const TimeOption = styled.div`
+    display: flex;
+    margin: 22px 0;
+`;
+const RadioOption = styled.input`
+    visibility: collapse;
+`;
+
+const LabelOption = styled.label`
+    font-family: 'Roboto', sans-serif;
+    font-size: 18px;
+    font-weight: 400;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #E8833A;
+    margin: 0 8px;
+    height: 40px;
+    width: 80px;
+    cursor: pointer;
+`;
