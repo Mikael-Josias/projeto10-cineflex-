93d878a72e213fa1c3177e296aaf00ceed9677ae
Mikael Josias <mikaeljosias2002@gmail.com>
2022-12-07 16:23:35 -0300

    refactor: make double verification to show schedule page

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,20 +6,13 @@ import Schedule from "./Schedule";
 
 export default function Content(){
     const [selectedMovie, setSelectedMovie] = useState(null);
-
-    const movie = {
-        id: 1,
-        title: "2067",
-        posterURL: "https://image.tmdb.org/t/p/w500/7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
-        overview: "A lowly utility worker is called to the future by a mysterious radio signal, he must leave his dying wife to embark on a journey that will force him to face his deepest fears in an attempt to change the fabric of reality and save humankind from its greatest environmental crisis yet.",
-        releaseDate: "2020-10-01T00:00:00.000Z",
-    };
+    const [selectedDate, setSelectedDate] = useState(null);
 
     return (
         <StyledContent>
             <ContentSubtitle>Selecione o {"filme"}</ContentSubtitle>
             {selectedMovie === null ? <Movies setSelectedMovie={setSelectedMovie} /> : null}
-            {selectedMovie !== null ? <Schedule movie={selectedMovie} /> : null}
+            {selectedMovie !== null && selectedDate === null ? <Schedule movie={selectedMovie} /> : null}
         </StyledContent>
     );
 }
