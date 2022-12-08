d8b154ebb5e8945046bf8209d61f0e66e9bba7ee
Mikael Josias <mikaeljosias2002@gmail.com>
2022-12-08 11:38:16 -0300

    feat: get available seats from api

--- src/components/Seats.js
+++ src/components/Seats.js
@@ -1,20 +1,33 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Seats(props){
     const {movieId, scheduleId} = useParams();
+
+    const [dayInfo, setDayInfo] = useState(null);
+    const [movieInfo, setMovieInfo] = useState(null);
+    const [seatsInfo, setSeatsInfo] = useState(null);
+
     const seatsUrl = `https://mock-api.driven.com.br/api/v8/cineflex/showtimes/${scheduleId}/seats`;
 
     useEffect(() => {
         const promisse = axios.get(seatsUrl);
-        promisse.then((p) => console.log(p.data));
+        promisse.then((p) => {
+            setDayInfo(p.data.day);
+            setMovieInfo(p.data.movie);
+            setSeatsInfo(p.data.seats);
+        });
         promisse.catch((p) => console.log(p.message));
     }, []);
 
+    if (setDayInfo === null) {
+        return <div>Carregando...</div>
+    }
+
     return (
         <>
-            seatsUrl
+            Footer
         </>
     );
 }
